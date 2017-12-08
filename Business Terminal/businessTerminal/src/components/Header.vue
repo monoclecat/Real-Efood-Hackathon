990bd7f9528a435145269f5a4ce3f8d56fe21a01
m-wessendorf <max.wessendorf@t-online.de>
2017-12-08 22:12:21 +0100

    Added Header Component

---
+++ Business Terminal/businessTerminal/src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header id="bt-header">
+    <b-navbar id="bt-navbar" toggleable="md" type="dark" variant="danger">
+
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+      <b-navbar-brand href="#">E-Food Business Terminal</b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <b-navbar-nav>
+          <b-nav-item href="#" disabled>Start</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+        
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
+  </header>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
